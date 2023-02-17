8d42cd31b80e84d50b491cd5c8cf3232e07d6bd0
Saptarsi Basu <basu.saptarsi91@gmail.com>
2023-02-17 13:17:40 +0530

    u

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -21,7 +21,7 @@ package_application() {
 
 package_deploy() {
 	#sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.yml --stack-name llypharma-data-execution --parameter-overrides "$(jq -j 'to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' params.json)"
-	SAM_PARAMETERS=$( cat ${CODEBUILD_SRC_DIR}/param.json | jq -r ' .Parameters | to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' )
+	SAM_PARAMETERS=$( cat ${CODEBUILD_SRC_DIR}/param.json | jq -r ' .Parameters | .Tags | to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' )
 	sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.yml --stack-name llypharma-data-execution --parameter-overrides $SAM_PARAMETERS
 }
 
