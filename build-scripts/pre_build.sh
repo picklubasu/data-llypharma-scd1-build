729233c44c05a995d1fd127b86d5d8002bafe3af
Saptarsi Basu <basu.saptarsi91@gmail.com>
2023-03-10 14:58:04 +0530

    Adding Data Generator Lambda

--- build-scripts/pre_build.sh
+++ build-scripts/pre_build.sh
@@ -10,6 +10,13 @@ S3_PACKAGE_CONFIG_FOLDER=${S3_PACKAGE_CONFIG_FOLDER}
 echo "Copying package files..."
 aws s3 cp $CONFIG_PACKAGE_FOLDER $S3_PACKAGE_CONFIG_FOLDER --recursive
 
+# Copy codes
+CODE_PACKAGE_FOLDER="$CODEBUILD_SRC_DIR/config/code/"
+S3_PACKAGE_CODE_FOLDER=${S3_PACKAGE_CODE_FOLDER}
+echo "Copying package files..."
+aws s3 cp $CODE_PACKAGE_FOLDER $S3_PACKAGE_CODE_FOLDER --recursive
+
+
 cd $STARTING_DIR
 unset STARTING_DIR
 echo "Completed pre_build - $(date)"
